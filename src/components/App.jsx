a7cb5330c10f548c8baf21796a111646bb6f5e24
Haris Sucipto <harisssucipto@gmail.com>
2017-05-24 10:23:50 +0700

    add timestamp input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ''
     }
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    console.log('this.state.dueDate', this.state.dueDate)
+    this.props.addReminder(this.state.text, this.state.dueDate);
   }
 
   deleteReminder(id) {
@@ -53,6 +55,12 @@ class App extends Component {
               className="form-control"
               placeholder="I have to..."
               onChange={(event) => this.setState({text: event.target.value})}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={(event) => this.setState({dueDate: event.target.value})}
+
             />
           </div>
           <button
