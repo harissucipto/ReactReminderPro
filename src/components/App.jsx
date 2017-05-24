c4e6e5b37b3d107d04dd0c7b3d68a25048c9429b
Haris Sucipto <harisssucipto@gmail.com>
2017-05-24 10:08:20 +0700

    reducing constant delete reminder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,8 +15,7 @@ class App extends Component {
   }
 
   deleteReminder(id) {
-    console.log('deleting in application', id);
-    console.log('this props', this.props)
+    this.props.deleteReminder(id);
   }
 
   renderReminder() {
